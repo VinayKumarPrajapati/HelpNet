e25e3b8431d4f097dadd718606d2cd3e869f5871
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-06-25 13:46:09 +0530

    created help models

---
+++ helper-client/models/Help.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+const helpSchema = new mongoose.Schema(
+  {
+    helperId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    helpSeekerId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    helperRating: {
+      type: Number,
+      minlength: 0,
+      maxlength: 5,
+      default: 0,
+    },
+    helpSeekerRating: {
+      type: Number,
+      minlength: 0,
+      maxlength: 5,
+      default: 0,
+    },
+    description: {
+      type: String,
+    },
+    paid: {
+      type: Boolean,
+      default: false,
+    },
+    completedStatus: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Help = mongoose.model('Help', helpSchema);
+
+module.exports = Help;
