a9ba40c8813f479d5479a66eef2b983eac93b5ae
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-06-25 14:54:57 +0530

    added middlewares into index

---
+++ helper-client/routes/authRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const { signup, login } = require('../controllers/auth');
+
+router.post('/login', login);
+router.post('/signup', signup);
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+  //console.log(req.user);
+  // const user = req.user;
+  // const token = user.generateAuthToken();
+  return res.redirect('/');
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+});
+
+module.exports = router;
---
+++ helper-client/services/pasword.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('../config/keys');
+const User = require('../models/User');
+
+// TODO -- OAUTH
+passport.serializeUser((user, done) => {
+  const token = user.generateAuthToken();
+  done(null, token);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/api/auth/google/callback',
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      //console.log("profile:", profile);
+      const user = await new User({
+        googleId: profile.id,
+        profileImageUrl: profile._json.picture,
+        email: profile._json.email,
+      }).save();
+      done(null, user);
+    }
+  )
+);
